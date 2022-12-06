1e85a292dda9e1b2f81b327cc68308026c81b8c7
Jason Garcia <jaygarcia0530@gmail.com>
2022-12-05 20:04:21 -0800

    added titles to motorcyclecard.jsx

--- src/components/MotorcycleCard.jsx
+++ src/components/MotorcycleCard.jsx
@@ -10,9 +10,13 @@ function MotorcycleCard(props) {
             <div>
                 <img src={motorcycle.image}/>
                 <h1>{motorcycle.make}</h1>
+                <p>Model</p>
                 <p>{motorcycle.model}</p>
+                <p>Year</p>
                 <p>{motorcycle.year}</p>
+                <p>CC</p>
                 <p>{motorcycle.cc}</p>
+                <p>Skill</p>
                 <p>{motorcycle.skills}</p>
             </div>
         )
