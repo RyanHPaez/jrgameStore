0edecc5508d623c89db5cbc4d8db391084bceddd
Jason Garcia <jaygarcia0530@gmail.com>
2022-12-05 18:29:37 -0800

    test for push

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -3,7 +3,7 @@ import React from "react";
 function Page2() {
   return (
     <div className="col-sm-12 text-center">
-      <h1 className="font-weight-light">Hello, this is Page2</h1>
+      <h1 className="font-weight-light">Hello, this is Test</h1>
     </div>
   );
 }
