f0f1c5ba0ba1202abe4945bf7e64676045b7578c
Jason Garcia <jaygarcia0530@gmail.com>
2022-12-05 19:11:54 -0800

    created motorcycle mock data

---
+++ src/mockdata/MotoData.js
@@ -0,0 +1,29 @@
+export const MotoData = [
+    {
+        model: "CBR1000",
+        make: "Honda",
+        year: "2023",
+        type: "Sports",
+        image: "",
+        cc: "1000",
+        skills: "Advanced"
+    },
+    {
+        model: "Iron Sportster",
+        make: "Harley Davidson",
+        year: "2019",
+        type: "Cruiser",
+        image: "",
+        cc: "883",
+        skills: "Moderate"
+    },
+    {
+        model: "z400",
+        make: "Kawasaki",
+        year: "2020",
+        type: "Sports",
+        image: "",
+        cc: "400",
+        skills: "Beginner"
+    }
+]
