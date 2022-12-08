5981d31da86af6ee64bf436d50ce82eb825d0ccc
ryanhpaez <solosnail@gmail.com>
2022-12-08 03:09:10 -0800

    bootstrap motodata img

--- src/components/MotorcycleCard.jsx
+++ src/components/MotorcycleCard.jsx
@@ -1,65 +1,59 @@
 import { Card } from "@mui/material";
-import img1 from "../images/cbr1000.jpg"// eslint-disable-next-line
-import img2 from "../images/sportster.jpg"// eslint-disable-next-line
-import img3 from "../images/z400.jpg"// eslint-disable-next-line
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import img1 from "../images/cbr1000.jpg"; // eslint-disable-next-line
+import img2 from "../images/sportster.jpg"; // eslint-disable-next-line
+import img3 from "../images/z400.jpg"; // eslint-disable-next-line
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 
 function MotorcycleCard(props) {
+  const { motorcycle } = props;
 
-    const { motorcycle } = props;
-
-        return (
-<div className="container">
-    <div className="row">
-            
-      <div className="hero-image ">
-        <div className="hero-type ">
-            <div className="info ">
-                <img src={motorcycle.image}
-                alt='motorcycles'
-                class="topbikes "/>
-                </div></div></div>
-                <Grid container rowSpacing={1} mt={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    {/* <Grid item xs={6}>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="hero-image ">
+            <div className="hero-type ">
+              <div className="info ">
+                <img
+                  src={motorcycle.image}
+                  alt="motorcycles"
+                  class="topbikes "
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Grid
+          container
+          rowSpacing={1}
+          mt={5}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          {/* <Grid item xs={6}>
                         
                     </Grid> */}
-                    <Grid item xs={6}>
-                        <h1>{motorcycle.make}</h1>
-                Model: 
-                {motorcycle.model}
-                    </Grid>
-                    <Grid item xs={6}>
-                        Year:
-                {motorcycle.year}
-                    </Grid>
-                    <Grid item xs={6}>
-                        Engine Size:
-                {motorcycle.cc}
-                    </Grid>
-                    <Grid item xs={6}>
-                        Skill:
-                {motorcycle.skills}
-                    </Grid>
-                </Grid>
-                {/* <div className="container">
-                <div className="row mt-4">
-                <div className="card col-lg-12 mt-4">
-                
-                
-                
-                
-                
-                
-            </div>
-            </div>
-            </div> */}
-            
-            
-            
-            </div>
-            </div>
-        )
+          <Grid item xs={6}>
+            <h1>{motorcycle.make}</h1>
+            Model:
+            {motorcycle.model}
+          </Grid>
+          <Grid item xs={6}>
+            Year:
+            {motorcycle.year}
+          </Grid>
+          <Grid item xs={6}>
+            Engine Size:
+            {motorcycle.cc}
+          </Grid>
+          <Grid item xs={6}>
+            Skill:
+            {motorcycle.skills}
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
-export default MotorcycleCard
+export default MotorcycleCard;
