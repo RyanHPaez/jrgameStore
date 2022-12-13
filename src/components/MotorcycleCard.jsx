9dd7a3b5694bc524dbe1fc775fa3073889ee31c9
Jason Garcia <jaygarcia0530@gmail.com>
2022-12-13 12:59:28 -0800

    test

--- src/components/MotorcycleCard.jsx
+++ src/components/MotorcycleCard.jsx
@@ -6,20 +6,20 @@ function MotorcycleCard(props) {
 
     const { motorcycle } = props;
 
-        return (
-            <div>
-                <img src={motorcycle.image}/>
-                <h1>{motorcycle.make}</h1>
-                <p>Model</p>
-                <p>{motorcycle.model}</p>
-                <p>Year</p>
-                <p>{motorcycle.year}</p>
-                <p>CC</p>
-                <p>{motorcycle.cc}</p>
-                <p>Skill</p>
-                <p>{motorcycle.skills}</p>
-            </div>
-        )
+    return (
+        <div>
+            <img src={motorcycle.image} />
+            <h1>{motorcycle.make}</h1>
+            <p>Model</p>
+            <p>{motorcycle.model}</p>
+            <p>Year</p>
+            <p>{motorcycle.year}</p>
+            <p>CC</p>
+            <p>{motorcycle.cc}</p>
+            <p>Skill</p>
+            <p>{motorcycle.skills}</p>
+        </div>
+    )
 }
 
 export default MotorcycleCard
